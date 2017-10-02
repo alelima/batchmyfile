b8b98bfbc414c7bef135e0869796d1789da818e0
alelima <alessandro@gmail.com>
2017-10-02 15:39:50 -0300

    Creating classes for unit test

---
+++ src/test/java/org/nitrox/batchmyfile/file/FileLineProcessorTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nitrox.batchmyfile.file;
+
+/**
+ *
+ * @author 03883182443
+ */
+public class FileLineProcessorTest {
+    
+    
+    public void getFieldsLineValuesTest() {
+        
+    }
+    
+}
